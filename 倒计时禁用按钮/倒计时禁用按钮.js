a30a774f5c3cd0755311bcbdc0e23eb1d73d915a
hjxool <691638334@qq.com>
2022-04-21 16:25:52 +0800

    updated

--- 倒计时禁用按钮/倒计时禁用按钮.js
+++ 倒计时禁用按钮/倒计时禁用按钮.js
@@ -20,19 +20,19 @@ let a = new Vue({
 	},
 	methods: {
 		count_down() {
-			// let timer = setInterval(() => {
-			// 	this.count--;
-			// 	this.ban = true;
-			// 	this.btn_text = `${this.count}s后重新发送`;
-			// 	if (this.count == 0) {
-			// 		this.btn_text = '重新发送验证码';
-			// 		this.ban = false;
-			// 		this.count = 10;
-			// 		clearInterval(timer);
-			// 	}
-			// }, 1000);
-			// return this.btn_text;
-			this.obj3.aaa = !this.obj3.aaa;
+			this.ban = true;
+			let timer = setInterval(() => {
+				this.btn_text = `${this.count}s后重新发送`;
+				this.count--;
+				if (this.count == 0) {
+					this.btn_text = '重新发送验证码';
+					this.ban = false;
+					this.count = 10;
+					clearInterval(timer);
+				}
+			}, 1000);
+			return this.btn_text;
+			// this.obj3.aaa = !this.obj3.aaa;
 		},
 	},
 	watch: {
@@ -46,9 +46,14 @@ let a = new Vue({
 		},
 	},
 });
-console.log(a.test);
-console.log(a.count_down);
-console.log(a.count_down());
-console.log(typeof a.test);
-console.log(typeof a.count_down);
-console.log(typeof a.count_down());
+// console.log(a.test);输出string
+// console.log(a.count_down);整个函数体
+// console.log(a.count_down());输出函数结果
+// console.log(typeof a.test);类型也是string
+// console.log(typeof a.count_down);类型function
+// console.log(typeof a.count_down());没有返回值就是undefined
+let array = ['daas', '12321'];
+function xxx(param) {
+	console.log(param);
+}
+xxx(...array);
